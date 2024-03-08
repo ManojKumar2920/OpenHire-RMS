71420d3ae6a61a5569243e6de2f06c0591a3f74d
BALA SANTHOSH <127101180+BALASANTHOSH01@users.noreply.github.com>
2024-03-08 15:44:18 +0530

    auth

--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const Signup = () => {
     return (
-        <div className="h-[90vh] mt-[20%]">
+        <div className=" mt-[9%]">
 
             <div className=" bg-blue-600 rounded-[50%] absolute -z-10 "></div>
 
